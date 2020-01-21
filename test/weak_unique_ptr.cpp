8f614a8ceb4034c8e826f2aac2080b2fc0e6e907
Jin M <jinmiao@utexas.edu>
2020-01-20 21:28:49 -0800

    add

---
+++ test/weak_unique_ptr.cpp
@@ -0,0 +1,10 @@
+
+// weak_ptr:
+// It has no ownership of the pointed object.
+// whether or not the pointed object to be deleted is non of my business.
+//
+// shared_ptr: 
+// it defines the ownership of the pointing object and are shared with other pointers
+//
+// unique_ptr:
+// lightweight smart pointer, exclusive ownership!
